7faa6dace3efd20eed32c8bee1111671e693b09d
Viktor Kovács <viktor.kovacs02@gmail.com>
2023-02-14 21:14:12 +0100

    Added SetupRequest

---
+++ src/Requests/Setup/SetupRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theshop\ApiClient\Requests\Setup;
+
+use Theshop\ApiClient\AbstractRequest;
+
+class SetupRequest extends AbstractRequest
+{
+    protected string $url = 'v1/setup';
+}
