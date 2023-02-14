50342fd1940104e7e671cb1649833803126d777c
Viktor Kovács <viktor.kovacs02@gmail.com>
2023-02-14 21:15:04 +0100

    Added SupplierSettingRequest

---
+++ src/Requests/Settings/SupplierSettingRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theshop\ApiClient\Requests\Settings;
+
+use Theshop\ApiClient\AbstractRequest;
+
+class SupplierSettingRequest extends AbstractRequest
+{
+    protected string $url = 'v1/settings/supplier';
+}
