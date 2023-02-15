e4181c3d969653b4675626742d50e86fd36f3c67
Viktor Kovács <viktor.kovacs02@gmail.com>
2023-02-15 17:49:59 +0100

    Added PacketaSettingRequest

---
+++ src/Requests/Settings/PacketaSettingRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theshop\ApiClient\Requests\Settings;
+
+use Theshop\ApiClient\AbstractRequest;
+
+class PacketaSettingRequest extends AbstractRequest
+{
+    protected string $url = 'v1/settings/packeta';
+}
