6740901dd49bdb7a5fdc4b0dc7e84f7ad5df7c2d
Robert Zuzik <zuzik@asdata.sk>
2023-03-22 15:11:33 +0100

    page-detail-to-v2

--- src/Requests/Pages/PageDetailRequest.php
+++ src/Requests/Pages/PageDetailRequest.php
@@ -6,18 +6,28 @@ use Theshop\ApiClient\AbstractRequest;
 
 class PageDetailRequest extends AbstractRequest
 {
-    protected string $url = 'v1/pages/detail';
+    protected string $url = 'v2/pages/detail';
 
     public function __construct(
         public readonly string $locale,
         public readonly string $currency,
-        public readonly string $slug,
+        public readonly int $limit = 1000,
+        public readonly int $offset = 0,
+        public readonly string $sort = 'score',
+        public readonly string $order = 'desc',
+        public readonly ?string $slug = null,
+        public readonly ?string $uuid = null,
     ) {
         $this->options = [
             'query' => [
                 'locale'   => $this->locale,
                 'currency' => $this->currency,
+                'limit'    => $this->limit,
+                'offset'   => $this->offset,
+                'sort'     => $this->sort,
+                'order'    => $this->order,
                 'slug'     => $this->slug,
+                'uuid'     => $this->uuid,
             ],
         ];
     }
