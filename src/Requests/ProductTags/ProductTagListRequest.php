1c7bdb43d9dccd7788f914a2e73430cd44e72231
Robert Zuzik <zuzik@asdata.sk>
2023-03-27 08:33:24 +0200

    product-tag-list-v2

--- src/Requests/ProductTags/ProductTagListRequest.php
+++ src/Requests/ProductTags/ProductTagListRequest.php
@@ -6,22 +6,24 @@ use Theshop\ApiClient\AbstractRequest;
 
 class ProductTagListRequest extends AbstractRequest
 {
-    protected string $url = 'v1/product-tags';
+    protected string $url = 'v2/product-tags';
 
     public function __construct(
         public readonly string $locale,
+        public readonly string $currency,
         public readonly string $sort = 'created_at',
         public readonly string $order = 'desc',
-        public readonly string $limit = '0',
+        public readonly string $limit = '1000',
         public readonly string $offset = '0',
     ) {
         $this->options = [
             'query' => [
-                'locale' => $this->locale,
-                'sort'   => $this->sort,
-                'order'  => $this->order,
-                'limit'  => $this->limit,
-                'offset' => $this->offset,
+                'locale'    => $this->locale,
+                'currency'  => $this->currency,
+                'sort'      => $this->sort,
+                'order'     => $this->order,
+                'limit'     => $this->limit,
+                'offset'    => $this->offset,
             ],
         ];
     }
